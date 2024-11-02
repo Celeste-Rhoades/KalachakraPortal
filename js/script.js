932b0e9eb82774d7d100311535cfcffbcbbb5328
Celeste-Rhoades <celesteisthebest82@gmail.com>
2024-11-02 17:47:57 -0600

    updated zoom feature on images

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
+const downloadIcon = document.getElementById("download-icon");
 
 btn.addEventListener("click", () => {
+  console.log("button clicked");
   btn.classList.toggle("open");
   nav.classList.toggle("flex");
   nav.classList.toggle("hidden");
+});
+
+mediumZoom(".zoom", {
+  margin: 50,
+  background: "#000",
 });
